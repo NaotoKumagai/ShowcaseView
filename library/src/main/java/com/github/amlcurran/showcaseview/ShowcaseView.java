e593d02b2681d08b82dd0b654be2e7d712f60246
Jyunji Kondo <j.kondo@mana.bo>
2014-12-01 14:01:37 +0900

    Fix out of memory error by calling garbege collection when bitmap is cleared

--- library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
+++ library/src/main/java/com/github/amlcurran/showcaseview/ShowcaseView.java
@@ -320,6 +320,7 @@ public class ShowcaseView extends RelativeLayout
         if (bitmapBuffer != null && !bitmapBuffer.isRecycled()) {
             bitmapBuffer.recycle();
             bitmapBuffer = null;
+            System.gc();
         }
     }
 
